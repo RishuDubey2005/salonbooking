33eeb3ac525b21279b3bef22d003eee543d7bb00
SUDHANSHU SHEKHAR <148328115+RishuDubey2005@users.noreply.github.com>
2025-11-23 22:50:42 +0530

    Add files via upload

---
+++ header.js
@@ -0,0 +1,6 @@
+fetch("header.html")
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("header").innerHTML = html;
+  })
+  .catch(err => console.error("Header load error:", err));
